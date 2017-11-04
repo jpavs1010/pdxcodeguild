f20c0aa6b07f10b60713f586905e20c13ac31683
Jessica <jlpavlinko@gmail.com>
2017-11-03 20:20:42 -0700

    load_data: compare pd.ExcelFile, pd.read_excel

--- capstone_project/capstone/management/commands/load_data.py
+++ capstone_project/capstone/management/commands/load_data.py
@@ -8,18 +8,33 @@ def erase_database():
     AccessData.objects.all().delete()
 
 
-# open xls (pick out relevant data)
+
 def open_data():
+    file = r'C:\Users\Jessica\PycharmProjects\pdxcodeguild\capstone_project\capstone\management\commands\food_environment_atlas.xls'
+    xls_file = pd.ExcelFile(file)
+
+    # df = pd.read_excel(file, converters={'FIPS': str})
+    # print(df['Food Environment Atlas data download'][1])
+    #print(xls_file.sheet_names)
+    print(xls_file.parse('ACCESS', converters={'FIPS': str})['FIPS'][0])
+
+
+# open xls (pick out relevant data)
+def open_data2():
     file = r'C:\Users\Jessica\PycharmProjects\pdxcodeguild\capstone_project\capstone\management\commands\food_environment_atlas.xls'
     # worksheets = ['ACCESS', 'HEALTH']
     # df = pd.read_excel(file, sheetname=worksheets, converters={'FIPS': str})
     # access_worksheet = df['ACCESS']
     # health_worksheet = df['HEALTH']
 
-    df1 = pd.read_excel(file, sheetname='ACCESS', converters={'FIPS': str})
+    df1 = pd.read_excel(file, sheetname='ACCESS', converters={'FIPS': str}, usecols=[0, 1, 2, 6])
     df2 = pd.read_excel(file, sheetname='HEALTH', converters={'FIPS': str}, usecols=[0, 4, 6])
 
     df_join = pd.merge(df1, df2, on='FIPS', how='inner')
+
+
+
+
 
     data_column = df_join['PCT_LACCESS_POP10']
     state_column = df_join['State']
@@ -27,6 +42,7 @@ def open_data():
     county_id_column = df_join['FIPS']
     diabetes_column = df_join['PCT_DIABETES_ADULTS13']
     obese_column = df_join['PCT_OBESE_ADULTS13']
+
 
     for i in range(len(data_column)):
         pct_access = data_column[i]
@@ -41,6 +57,10 @@ def open_data():
         all_data.save()
 
         print(f'{((i/len(data_column))*100)}%')
+
+    # df_corr = pd.DataFrame(df_join, columns=['PCT_LACCESS_POP10', 'PCT_DIABETES_ADULTS13', 'PCT_OBESE_ADULTS13'])
+    # df_corr['PCT_LACCESS_POP10'].corr(df_corr['PCT_DIABETES_ADULTS13'], method='spearman')
+    # df_corr['PCT_LACCESS_POP10'].corr(df_corr['PCT_OBESE_ADULTS13'], method='spearman')
 
 class Command(BaseCommand):
 
